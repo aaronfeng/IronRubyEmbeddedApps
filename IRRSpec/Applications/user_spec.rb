f15d52b8d3fb7ddb49a3f3c54ed1f2abae4a538d
Aaron Feng <aaron.feng@gmail.com>
2010-05-03 10:22:50 -0400

    added a rspec example project

---
+++ IRRSpec/Applications/user_spec.rb
@@ -0,0 +1,11 @@
+﻿require 'spec'
+require 'spec/autorun'
+
+class User
+end
+
+describe User do
+  it "should work" do
+    1.should == 1
+  end
+end
---
+++ IRRSpec/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IREmbeddedApp;
+
+namespace IRRSpec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var er = new EmbeddedRuby();
+                var resources = er.AddAssembly("IREmbeddedLibraries");
+                resources.Mount("Files/ironruby");
+                resources.Mount("Files/rspec-1.3.0/lib");
+                resources.Mount("Files/site_ruby/1.8");
+                resources.Mount("Files/1.8");
+
+                er.Mount("Applications");
+                er.Run("user_spec.rb", args);
+            }
+            catch (Exception)
+            {
+              // not sure why when rspec exits throws an exception
+            }
+
+            Console.WriteLine("Press Enter to Exit");
+            Console.ReadLine();
+        }
+    }
+}
